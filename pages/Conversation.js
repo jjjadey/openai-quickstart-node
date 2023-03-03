0b55c8446d2ddf180bba26152d2d2e60a0bc31ff
jjjadey <nattakan.premp@gmail.com>
2023-03-03 20:20:26 +0700

    Conversation example

--- pages/Conversation.js
+++ pages/Conversation.js
@@ -3,11 +3,13 @@ import { useState } from "react";
 import styles from "./index.module.css";
 
 export default function Conversation() {
-    const [result, setResult] = useState();
+    const [isLoading, setIsLoading] = useState(false);
+    const [result, setResult] = useState([]);
     const [conversationInput, setConversationInput] = useState("");
 
     async function onSubmitConversation(event) {
         event.preventDefault();
+        setIsLoading(true);
         try {
             const response = await fetch("/api/conversation", {
                 method: "POST",
@@ -22,8 +24,13 @@ export default function Conversation() {
                 throw data.error || new Error(`Request failed with status ${response.status}`);
             }
 
-            setResult(data.result);
-            setConversationInput("");
+            console.log(data.result)
+            // setResult(data.result);
+
+            console.log(JSON.parse(data.result))
+            setResult(JSON.parse(data.result));
+            // setConversationInput("");
+            setIsLoading(false);
         } catch (error) {
             // Consider implementing your own error handling logic here
             console.error(error);
@@ -35,16 +42,30 @@ export default function Conversation() {
         <>
             <h3>Conversation</h3>
             <form onSubmit={onSubmitConversation}>
-                <input
+                <textarea
                     type="text"
                     name="conversation"
                     placeholder="How can I help you today?"
                     value={conversationInput}
                     onChange={(e) => setConversationInput(e.target.value)}
+                    style={{
+                        height: '200px',
+                    }}
                 />
                 <input type="submit" value="Submit" />
             </form>
-            <div className={styles.result}>{result}</div>
+
+
+            {isLoading ? <p>Loading...</p> :
+                // <div className={styles.result}>{result}</div>
+                <div style={{ textAlign: "left" }}>
+                    <ol>
+                        {result && result.map((data) => (
+                            <li key={data.id}>{data.name} - {data.location}</li>
+                        ))}
+                    </ol>
+                </div>
+            }
         </>
     )
 }
--- pages/api/conversation.js
+++ pages/api/conversation.js
@@ -26,10 +26,13 @@ export default async function (req, res) {
     return;
   }
 
+  // I want to eat korean BBQ  and ice-cream. After that I want to see sunset near the river.  please recommend cafe and restaurant near Wat mangkon MRT Thailand
   try {
     const completion = await openai.createCompletion({
       model: "text-davinci-003",
-      prompt: `The following is a conversation with an AI assistant. The assistant is helpful, creative, clever, and very friendly.\n\nHuman: Hello, who are you?\nAI: I am an AI created by OpenAI. How can I help you today?\nHuman:${question}\n\nAI:`,
+      // prompt: `Human:${question}\n\nAI:`, //string
+      // prompt: `Human:${question}. return result into array of object form\n\nAI:`, //array
+      prompt: `Human:${question}. return result into array of object form {"id": number, "name": string, "location": string}\n\nAI:`, //set key
       temperature: 0.9,
       max_tokens: 150,
       top_p: 1,
@@ -43,7 +46,7 @@ export default async function (req, res) {
       result = result + choice.text;
     });
     console.log(completion.data)
-    console.log(result)
+
     res.status(200).json({ result: result });
   } catch (error) {
     // Consider adjusting the error handling logic for your use case
